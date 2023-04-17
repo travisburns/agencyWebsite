0df5450d2334129e1a7ed7316e6669ea65161d73
travis <travisjb0@gmail.com>
2023-04-17 01:42:36 -0700

    First Commit after update to main branch. Added Button

--- agencysite/src/components/hero/Hero.jsx
+++ agencysite/src/components/hero/Hero.jsx
@@ -5,13 +5,34 @@ function Hero() {
   
   
     return (
-    <div>
+    <div className='h-[40rem]'>
       <img className='w-full' src={heroCover} alt="heroImage"/> 
-      <h1 className='font-black text-white lg:text-[60px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 text-white relative  left-40 bottom-[35rem] h-17 w-[30rem]'>Bridge Your Technological Gap Today!</h1>
-     
-     
-     
-    </div>
+      <h1 className='font-black 
+      text-white 
+      lg:text-[60px] 
+      sm:text-[60px] 
+      xs:text-[50px] 
+      text-[40px] 
+      lg:leading-[98px] 
+      mt-2 text-white relative  
+      left-40 bottom-[35rem] 
+      h-17 
+      w-[30rem]
+      '>
+        Bridge Your Technological Gap Today!</h1>
+        <button className='
+         bg-indigo-950
+         p-5 
+         rounded-2xl 
+         sm:w-[130px] 
+         text-white
+         relative
+         left-40 
+         bottom-[35rem] 
+        
+        '>
+          Get Started!</button>
+     </div>
     
   )
 }
