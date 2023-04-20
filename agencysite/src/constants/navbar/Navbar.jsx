92420dd12269ff0c37328d59afc3a7b23b2cb032
travis <travisjb0@gmail.com>
2023-04-19 22:39:07 -0700

    Made navbar responsive on all screen sizes and made the font size more organized,clean and efficient

--- agencysite/src/constants/navbar/Navbar.jsx
+++ agencysite/src/constants/navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className= 'w-full flex items-center py-5 fixed top-0 z-20 '
+      className= 'w-full flex items-center py-5 fixed top-0 z-20 bg-slate-950'
         
       
     >
@@ -41,10 +41,13 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Travis Burns &nbsp;
-            <span className='sm:block hidden'> | Web Developer</span>
+          <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
+          <p className='text-white text-[13px] font-bold cursor-pointer flex 
+          sm:text-sm
+          
+          
+          '>
+           Eugene Web Development
           </p>
         </Link>
 
@@ -54,7 +57,7 @@ const Navbar = () => {
               key={nav.id}
               className={`${
                 active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              } hover:text-white text-[18px] font-medium cursor-pointer sm:text-sm`}
               onClick={() => setActive(nav.title)}
             >
               <a href={`#${nav.id}`}>{nav.title}</a>
@@ -75,11 +78,11 @@ const Navbar = () => {
               !toggle ? "hidden" : "flex"
             } p-6 bg-indigo-950 text-white absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4 '>
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
-                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  className={`font-poppins font-medium cursor-pointer text-[16px]  ${
                     active === nav.title ? "text-white" : "text-secondary"
                   }`}
                   onClick={() => {
