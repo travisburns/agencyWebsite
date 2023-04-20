9f2745f1aca225cb11410b31d28e0b934a9be6b3
travis <travisjb0@gmail.com>
2023-04-19 22:19:10 -0700

    Made Navbar Mobile Responsive and implemented a hook that sets navlinks to a button. The button uses a onclick to display links in vertical fasfashion

--- agencysite/src/constants/navbar/Navbar.jsx
+++ agencysite/src/constants/navbar/Navbar.jsx
@@ -1,40 +1,101 @@
+import React, { useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
 import logo from '../../assets/eugenewebdevlogo.png'
+import menu from '../../assets/menu.svg';
+import close from '../../assets/close.svg'
+
+import { navLinks } from '../navlinks.js';
 
 const Navbar = () => {
-  
-    return (
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
-  <nav className=' w-full flex items-center py-1 fixed top-0 z-20 bg-slate-950' >
-    <div className=' flex justify-between items-center max-w-7xl mx-auto'>
-      <Link 
-      to="/"
-      className="flex items-center gap-2"
-      onClick={() => {
-        setActive("");
-        window.scrollTo(0,0)
-      }}
-      >
-      <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-      <p className='text-white text-[18px] font-bold cursor-pointer flex'>
-        Eugene Web Development
-      </p>
-      </Link>
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-      <ul className="list-none hidden sm:flex flex-row gap-10 text-white font-semibold">
-        <li>Services</li>
-        <li>Work</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+    window.addEventListener("scroll", handleScroll);
 
-    </div>
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
+  return (
+    <nav
+      className= 'w-full flex items-center py-5 fixed top-0 z-20 '
+        
+      
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            Travis Burns &nbsp;
+            <span className='sm:block hidden'> | Web Developer</span>
+          </p>
+        </Link>
 
+        <ul className='list-none hidden sm:flex flex-row gap-10 text-white'>
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
 
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
+          />
 
-      </nav>
-  )
-}
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 bg-indigo-950 text-white absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4 '>
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
