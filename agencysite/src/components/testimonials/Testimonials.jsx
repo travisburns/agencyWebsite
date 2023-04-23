6a4c4360e4d66f839f8c5f38d34ff09e08eb7645
travis <travisjb0@gmail.com>
2023-04-22 22:40:00 -0700

    Added slight responsive design on 1280px for reviews section. Still needs work.

--- agencysite/src/components/testimonials/Testimonials.jsx
+++ agencysite/src/components/testimonials/Testimonials.jsx
@@ -12,27 +12,7 @@ const Testimonials = () => {
             <h4 className='testimonials_title  text-white text-[30px] font-black mb-10'>Testimonials</h4>
             <p className='testimonials_grabber font-black mb-10'>Dont't take our word for it. Over 100+ people trust us.</p>
             <div classname='testimonials_scroller flex flex-wrap'>
-                <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5  mb-5 left-[4rem]'>
-                    <p className='testimonials_scroller-item_title  mb-10 pt-5 width-5]'>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Doloremque eaque voluptates mollitia ut accusantium exercitationem.
-                    </p>
-                    <div className='testimonials_scroller-item_data'>
-
-                    
-                    <div className='testimonials_scroller-item_clientInfo flex'>
-                    
-                    <img src={testimonialImg} className='testimonials_scroller-item_clientInfo-img h-[3rem] w-[3rem]'/>
-                    <div className='testimonials_scroller-item_clientInfo-text relative left-[.5rem]'>
-                    <div className='testimonials_scroller-item_clientInfo-name'>Mickael Grants</div>
-                    <div className='testimonials_scroller-item_clientInfo-role text-[9px]'>CEO of Apple's to oranges</div>
-                    </div>
-                    </div>
-                    </div>
-                </div>
-
-
-                <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem]'>
+            <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem] sm:left-[8rem] xl:left-[19rem]'>
                     <p className='testimonials_scroller-item_title  mb-10 pt-5'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                         Doloremque eaque voluptates mollitia ut accusantium exercitationem.
@@ -42,17 +22,17 @@ const Testimonials = () => {
                     
                     <div className='testimonials_scroller-item_clientInfo flex'>
                     
-                    <img src={testimonialImg} className='testimonials_scroller-item_clientInfo-img h-[3rem] w-[3rem]'/>
+                    <img src={testimonialImg} className='testimonials_scroller-item_clientInfo-img h-[3rem] w-[3rem] sm:h-[6rem] sm:w-[6rem]'/>
                     <div className='testimonials_scroller-item_clientInfo-text relative left-[.5rem]'>
-                    <div className='testimonials_scroller-item_clientInfo-name'>Mickael Grants</div>
-                    <div className='testimonials_scroller-item_clientInfo-role text-[9px]'>CEO of Apple's to oranges</div>
+                    <div className='testimonials_scroller-item_clientInfo-name sm:text-[25px]'>Mickael Grants</div>
+                    <div className='testimonials_scroller-item_clientInfo-role text-[9px] sm:text-[20px]'>CEO of Apple's to oranges</div>
                     </div>
                     </div>
                     </div>
                 </div>
 
 
-                <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem]'>
+                <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem] sm:left-[8rem] xl:left-[19rem]'>
                     <p className='testimonials_scroller-item_title  mb-10 pt-5'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                         Doloremque eaque voluptates mollitia ut accusantium exercitationem.
@@ -62,10 +42,30 @@ const Testimonials = () => {
                     
                     <div className='testimonials_scroller-item_clientInfo flex'>
                     
-                    <img src={testimonialImg} className='testimonials_scroller-item_clientInfo-img h-[3rem] w-[3rem]'/>
+                    <img src={testimonialImg} className='testimonials_scroller-item_clientInfo-img h-[3rem] w-[3rem] sm:h-[6rem] sm:w-[6rem]'/>
                     <div className='testimonials_scroller-item_clientInfo-text relative left-[.5rem]'>
-                    <div className='testimonials_scroller-item_clientInfo-name'>Mickael Grants</div>
-                    <div className='testimonials_scroller-item_clientInfo-role text-[9px]'>CEO of Apple's to oranges</div>
+                    <div className='testimonials_scroller-item_clientInfo-name sm:text-[25px]'>Mickael Grants</div>
+                    <div className='testimonials_scroller-item_clientInfo-role text-[9px] sm:text-[20px]'>CEO of Apple's to oranges</div>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+
+
+                <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem] sm:left-[8rem] xl:left-[19rem]'>
+                    <p className='testimonials_scroller-item_title  mb-10 pt-5'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Doloremque eaque voluptates mollitia ut accusantium exercitationem.
+                    </p>
+                    <div className='testimonials_scroller-item_data'>
+
+                    
+                    <div className='testimonials_scroller-item_clientInfo flex'>
+                    
+                    <img src={testimonialImg} className='testimonials_scroller-item_clientInfo-img h-[3rem] w-[3rem] sm:h-[6rem] sm:w-[6rem]'/>
+                    <div className='testimonials_scroller-item_clientInfo-text relative left-[.5rem]'>
+                    <div className='testimonials_scroller-item_clientInfo-name sm:text-[25px]'>Mickael Grants</div>
+                    <div className='testimonials_scroller-item_clientInfo-role text-[9px] sm:text-[20px]'>CEO of Apple's to oranges</div>
                     </div>
                     </div>
                     </div>
