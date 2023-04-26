acb3853a31415bea20998c120c9058ef6d93ab88
Travis <travisjb0@gmail.com>
2023-04-25 17:13:32 -0700

    implemented better responsive design for testimonials section

--- agencysite/src/components/testimonials/Testimonials.jsx
+++ agencysite/src/components/testimonials/Testimonials.jsx
@@ -11,8 +11,8 @@ const Testimonials = () => {
         <div className='testimonials text-center font-Poppins text-white'>
             <h4 className='testimonials_title  text-white text-[30px] font-black mb-10'>Testimonials</h4>
             <p className='testimonials_grabber font-black mb-10'>Dont't take our word for it. Over 100+ people trust us.</p>
-            <div classname='testimonials_scroller flex flex-wrap'>
-            <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem] sm:left-[8rem] xl:w-[37rem] 2xl:w-[70.2rem] 2xl:left-[24rem]'>
+            <div classname='testimonials_scroller flex flex-wrap relative mb:left-[0rem] md:left-[2rem] '>
+            <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem] sm:left-[8rem] lg:left-[13rem] 2xl:w-[70.2rem] l-desktop:left-[26rem]'>
                     <p className='testimonials_scroller-item_title  mb-10 pt-5'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                         Doloremque eaque voluptates mollitia ut accusantium exercitationem.
@@ -25,14 +25,14 @@ const Testimonials = () => {
                     <img src={testimonialImg} className='testimonials_scroller-item_clientInfo-img h-[3rem] w-[3rem] sm:h-[6rem] sm:w-[6rem] xl:w-[17rem] xl:h-[11rem]'/>
                     <div className='testimonials_scroller-item_clientInfo-text relative left-[.5rem] xl:w-[23rem] xl:left-[5.5rem] xl:top-[1rem] 2xl:w-[37rem]'>
                     <div className='testimonials_scroller-item_clientInfo-name sm:text-[25px] xl:text-[46px]'>Mickael Grants</div>
-                    <div className='testimonials_scroller-item_clientInfo-role text-[9px] sm:text-[20px] xl:text-[27px]'>CEO of Apple's to oranges</div>
+                    <div className='testimonials_scroller-item_clientInfo-role text-[9px] sm:text-[20px] lg:w-[30rem] xl:text-[27px]'>CEO of Apple's to oranges</div>
                     </div>
                     </div>
                     </div>
                 </div>
 
 
-                <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem] sm:left-[8rem] xl:left-[24rem]'>
+                <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem] sm:left-[8rem] lg:left-[13rem] 2xl:w-[70.2rem] l-desktop:left-[26rem]'>
                     <p className='testimonials_scroller-item_title  mb-10 pt-5'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                         Doloremque eaque voluptates mollitia ut accusantium exercitationem.
@@ -45,7 +45,7 @@ const Testimonials = () => {
                     <img src={testimonialImg} className='testimonials_scroller-item_clientInfo-img h-[3rem] w-[3rem] sm:h-[6rem] sm:w-[6rem] xl:w-[17rem] xl:h-[11rem]'/>
                     <div className='testimonials_scroller-item_clientInfo-text relative left-[.5rem] xl:w-[23rem] xl:left-[5.5rem] xl:top-[1rem] xl:w-[37rem]'>
                     <div className='testimonials_scroller-item_clientInfo-name sm:text-[25px] xl:text-[46px]'>Mickael Grants</div>
-                    <div className='testimonials_scroller-item_clientInfo-role text-[9px] sm:text-[20px] xl:text-[27px]'>CEO of Apple's to oranges</div>
+                    <div className='testimonials_scroller-item_clientInfo-role text-[9px] sm:text-[20px] lg:w-[30rem] xl:text-[27px]'>CEO of Apple's to oranges</div>
                     </div>
                     </div>
                     </div>
@@ -53,7 +53,7 @@ const Testimonials = () => {
 
 
 
-                <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem] sm:left-[8rem] xl:left-[24rem]'>
+                <div className='testimonials_scroller-item rounded-lg bg-slate-950  relative w-3/5 mb-5 left-[4rem] sm:left-[8rem] lg:left-[13rem]  2xl:w-[70.2rem] l-desktop:left-[26rem]'>
                     <p className='testimonials_scroller-item_title  mb-10 pt-5'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                         Doloremque eaque voluptates mollitia ut accusantium exercitationem.
@@ -66,7 +66,7 @@ const Testimonials = () => {
                     <img src={testimonialImg} className='testimonials_scroller-item_clientInfo-img h-[3rem] w-[3rem] sm:h-[6rem] sm:w-[6rem] xl:w-[17rem] xl:h-[11rem]'/>
                     <div className='testimonials_scroller-item_clientInfo-text relative left-[.5rem] xl:w-[23rem] xl:left-[5.5rem] xl:top-[1rem] xl:w-[37rem]'>
                     <div className='testimonials_scroller-item_clientInfo-name sm:text-[25px] xl:text-[46px]'>Mickael Grants</div>
-                    <div className='testimonials_scroller-item_clientInfo-role text-[9px] sm:text-[20px] xl:text-[27px]'>CEO of Apple's to oranges</div>
+                    <div className='testimonials_scroller-item_clientInfo-role text-[9px] sm:text-[20px] lg:w-[30rem] xl:text-[27px]'>CEO of Apple's to oranges</div>
                     </div>
                     </div>
                     </div>
