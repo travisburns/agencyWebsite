cd961ada06a76a7f0ed422b9784d360db66aaaf6
Travis <travisjb0@gmail.com>
2023-05-10 17:50:30 -0700

    Added fade in animation for contact section

--- agencysite/src/components/contact/Contact.jsx
+++ agencysite/src/components/contact/Contact.jsx
@@ -1,13 +1,18 @@
-
 import React from 'react'
+import { motion, useScroll } from "framer-motion"
 
 const Contact = () => {
   return (
       <div className=' relative w-[80%] left-[2rem] sm:left-[4rem] md:left-[5rem] lg:left-[9rem]  xl:mt-12   gap-10 overflow-hidden l-desktop:left-[15rem] 2xl:pb-[10rem]'>
         <p className='text-white font-Poppins text-[1.8rem] relative bottom-[600rem]' >Get in touch</p>
         <h3 className='text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center'>Contact.</h3>
-        <form 
+        <motion.form 
+          style={{position: "relative"}}
+          initial={{ bottom: "80px", opacity: 0 }}
           
+          whileInView={{ bottom: "0px", opacity: 1 }}
+          viewport={{ once: false}}
+          transition={{ duration: 1, delay: 0.5, staggerChildren: 0.2, delayChildren: 0.5 }}
     
           className="mt-12 flex flex-col gap-8 bg-slate-950"
         >
@@ -56,7 +61,7 @@ const Contact = () => {
             >
               
             </button>
-          </form>
+          </motion.form>
         
 
      
