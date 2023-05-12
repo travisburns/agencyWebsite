350a243a25cb6aa8d7f2a1b5829d429bf98ade63
Travis <travisjb0@gmail.com>
2023-05-11 17:28:01 -0700

    Added Variant basic code. However it is not working as expected. Currently going to do more research in order to fix and make childrenstaggerwork with variants

--- agencysite/src/components/work/Work.jsx
+++ agencysite/src/components/work/Work.jsx
@@ -4,7 +4,22 @@ import { motion, useScroll } from "framer-motion"
 
 
 const Work = () => {
-  
+   
+    const workTitleContainer = {
+        hidden: { opacity: 0 },
+        show: {
+          opacity: 1,
+          transition: {
+            delayChildren: 0.5
+          }
+        }
+      }
+
+      const item = {
+        hidden: { opacity: 0 },
+        show: { opacity: 1 }
+      }
+      
 
     return (
         <section className='sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0 2xl:pb-[10rem]' >
@@ -40,11 +55,17 @@ const Work = () => {
             
             />
             </div>
-            <div className='mt-5'>
-                <h3 className='text-white font-bold text-[24px]'>Title</h3>
+            <motion.div
+            variants={workTitleContainer} 
+            initial="hidden"
+            animate="show"
+            className='mt-5'>
+                <motion.h3
+                variants={item}
+                 className='text-white font-bold text-[24px]'>Title</motion.h3>
                 <p className='mt-2 text-white text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Quia, minima velit repudiandae minus debitis voluptatibus suscipit excepturi quisquam rerum numquam.</p>
-            </div>
+            </motion.div>
             <div className='mt-4 flex flex-wrap gap-2'>
                 <p className='text-[14px] text-blue-500'>React</p>
                 <p className='text-[14px] text-green-500'>MongoDb</p>
